b03703280aa1beea4a8faccc63dde8d845ee51df
DeepakSharma90277 <dsharma90277@gmail.com>
2026-02-05 19:08:26 +0530

    Update port

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,31 +1,31 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,      // smtp.gmail.com
-      port: 587,                        // ✅ VERY IMPORTANT
-      secure: false,                    // must be false for 587
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: false,   // IMPORTANT
       auth: {
-        user: process.env.MAIL_USER,    // your gmail
-        pass: process.env.MAIL_PASS,    // GOOGLE APP PASSWORD
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
       },
-    })
+    });
 
     let info = await transporter.sendMail({
       from: `"Techvidya" <${process.env.MAIL_USER}>`,
       to: email,
       subject: title,
       html: body,
-    })
+    });
 
-    console.log("✅ Email sent:", info.messageId)
-    return info
+    console.log("Email sent:", info.response);
+    return info;
 
   } catch (error) {
-    console.error("❌ Email Error:", error)
-    throw error
+    console.log("❌ Email Error:", error);
+    return error.message;
   }
-}
+};
 
-module.exports = mailSender
+module.exports = mailSender;
